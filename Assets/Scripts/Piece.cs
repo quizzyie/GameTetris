ce7eb922d126a93852c4d2789c73ed029123d639
Quyt <92991639+ChrisNguyen25@users.noreply.github.com>
2024-03-24 04:34:22 +0700

    update

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Piece : MonoBehaviour{
+    public Board board;
+    public TetrominoData data { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    public void Initialize (Board board, Vector3Int position, TetrominoData data) {
+        this.board = board;
+        this.position = position;
+        this.data = data;
+
+        if (this.cells == null) {
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < data.cells.Length; i++) {
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
+    }
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Move(Vector2Int.left);
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            Move(Vector2Int.right);
+        }
+    
+    }
+
+    private bool Move(Vector2Int translation) {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.board.IsValidPosition(this, newPosition);
+
+        if(valid) {
+            this.position = newPosition;
+        }
+        return valid;
+    }
+}
+   
+
